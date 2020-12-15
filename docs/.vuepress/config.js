cd0810c5f1e9bbb2c7f4f4f56f53d54a8786c46b
robbyhunt <robbyshunt@gmail.com>
2020-12-15 16:34:42 +0000

    removed images + added meta image

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,60 +18,49 @@ module.exports = {
   },
   head: [
     ["link", { rel: "manifest", href: "/manifest.json" }],
-    [
-      "link",
-      { rel: "apple-touch-icon", href: "/assets/favicon/icon-180x180.png" }
-    ],
+    ["link", { rel: "apple-touch-icon", href: "/assets/favicon/icon-180x180.png" }],
     ["link", { rel: "icon", href: "/assets/favicon/icon-512x512.png" }],
-    ["meta", { name: "theme-color", content: "#2196f3" }]
+    ["meta", { name: "theme-color", content: "#2196f3" }],
+    ["meta", { name: "og:image", content: "/assets/img/bg-home-white.jpg" }]
   ],
-  locales: {
-    "/": {
-      title: "Mardha",
-      description: "Fantasy Setting for Roleplaying Games"
-    }
-  },
+  title: "Mardha",
+  description: "Fantasy Setting for story-telling and the Savage Worlds roleplaying game",
 
   // Theme Config
   themeConfig: {
     logo: "/assets/img/logo-aumyr-world.svg",
     docsDir: "docs",
     sidebar: "auto",
-    locales: {
-      "/": {
-        repoLabel: "Contribute",
-        nav: [
-          { text: "Geography",
+    nav: [
+      { text: "Geography",
+        items: [
+          { text: "Cities", link: "/geography/cities/" },
+          { text: "Continents",
             items: [
-              { text: "Cities", link: "/geography/cities/" },
-              { text: "Continents",
-                items: [
-                  { text: "Lorien", link: "/geography/lorien/" },
-                  { text: "Avestan", link: "/geography/avestan/" },
-                  { text: "Unnamed", link: "/geography/unnamed/" },
-                ],
-              },
+              { text: "Lorien", link: "/geography/lorien/" },
+              { text: "Avestan", link: "/geography/avestan/" },
+              { text: "Unnamed", link: "/geography/unnamed/" },
             ],
           },
-          {
-            text: "Society",
-              items : [
-                {text: "Race", link: "/society/race/"},
-                {text: "Religion", link: "/society/religion/"},
-              ]
-          },
-          {text: "Magic", link: "/magic/magic/"},
-          {
-            text: "Almanac",
-            items: [
-              { text: "Characters", link: "/almanac/characters/" },
-              { text: "Creatures", link: "/almanac/creatures/" },
-              { text: "Items", link: "/almanac/items/" },
-            ]
-          }
+        ],
+      },
+      {
+        text: "Society",
+          items : [
+            {text: "Race", link: "/society/race/"},
+            {text: "Religion", link: "/society/religion/"},
+          ]
+      },
+      {text: "Magic", link: "/magic/magic/"},
+      {
+        text: "Almanac",
+        items: [
+          { text: "Characters", link: "/almanac/characters/" },
+          { text: "Creatures", link: "/almanac/creatures/" },
+          { text: "Items", link: "/almanac/items/" },
         ]
       }
-    }
+    ]
   },
 
   // Plugins
